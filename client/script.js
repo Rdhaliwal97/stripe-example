ad1f041ccb6053ae1345fd32ad40f4688b87ebac
rDhaliwal97 <rdhaliwal97@outlook.com>
2019-11-30 10:51:45 +0530

    working template added

--- client/script.js
+++ client/script.js
@@ -1,65 +1,182 @@
 var stripe;
-var checkoutSessionId;
 
-var setupElements = function() {
-  fetch("/public-key", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
+var stripeElements = function(publicKey) {
+  stripe = Stripe(publicKey);
+  var elements = stripe.elements();
+
+  // Element styles
+  var style = {
+    base: {
+      fontSize: '16px',
+      color: '#32325d',
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif',
+      fontSmoothing: 'antialiased',
+      '::placeholder': {
+        color: 'rgba(0,0,0,0.4)'
+      }
     }
-  })
-    .then(function(result) {
-      return result.json();
-    })
-    .then(function(data) {
-      stripe = Stripe(data.publicKey);
-    });
-};
+  };
 
-var createCheckoutSession = function(isBuyingSticker) {
-  fetch("/create-checkout-session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ isBuyingSticker })
-  })
-    .then(function(result) {
-      return result.json();
-    })
-    .then(function(data) {
-      checkoutSessionId = data.checkoutSessionId;
-    });
-};
-setupElements();
-createCheckoutSession(false);
+  var card = elements.create('card', { style: style });
 
-document
-  .querySelector('input[name="subscribe"]')
-  .addEventListener("change", function(evt) {
-    if (this.checked) {
-      createCheckoutSession(true);
-      document.querySelector(".order-total").textContent = "$17.00"; // Because they are buying the extra item
-    } else {
-      createCheckoutSession(false);
-      document.querySelector(".order-total").textContent = "$14.00"; // Not buying the extra item
-    }
+  card.mount('#card-element');
+
+  // Element focus ring
+  card.on('focus', function() {
+    var el = document.getElementById('card-element');
+    el.classList.add('focused');
   });
 
-document.querySelector("#submit").addEventListener("click", function(evt) {
-  evt.preventDefault();
-  // Initiate payment
+  card.on('blur', function() {
+    var el = document.getElementById('card-element');
+    el.classList.remove('focused');
+  });
+
+  document.querySelector('#submit').addEventListener('click', function(evt) {
+    evt.preventDefault();
+    changeLoadingState(true);
+    // Initiate payment
+    createPaymentMethodAndCustomer(stripe, card);
+  });
+};
+
+function showCardError(error) {
+  changeLoadingState(false);
+  // The card was declined (i.e. insufficient funds, card has expired, etc)
+  var errorMsg = document.querySelector('.sr-field-error');
+  errorMsg.textContent = error.message;
+  setTimeout(function() {
+    errorMsg.textContent = '';
+  }, 8000);
+}
+
+var createPaymentMethodAndCustomer = function(stripe, card) {
+  var cardholderEmail = document.querySelector('#email').value;
   stripe
-    .redirectToCheckout({
-      sessionId: checkoutSessionId
+    .createPaymentMethod('card', card, {
+      billing_details: {
+        email: cardholderEmail
+      }
     })
     .then(function(result) {
-      console.log("error");
-      // If `redirectToCheckout` fails due to a browser or network
-      // error, display the localized error message to your customer
-      // using `result.error.message`.
-    })
-    .catch(function(err) {
-      console.log(err);
+      if (result.error) {
+        showCardError(result.error);
+      } else {
+        createCustomer(result.paymentMethod.id, cardholderEmail);
+      }
     });
-});
+};
+
+async function createCustomer(paymentMethod, cardholderEmail) {
+  return fetch('/create-customer', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: cardholderEmail,
+      payment_method: paymentMethod
+    })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(subscription => {
+      handleSubscription(subscription);
+    });
+}
+
+function handleSubscription(subscription) {
+  const { latest_invoice } = subscription;
+  const { payment_intent } = latest_invoice;
+
+  if (payment_intent) {
+    const { client_secret, status } = payment_intent;
+
+    if (status === 'requires_action' || status === 'requires_payment_method') {
+      stripe.confirmCardPayment(client_secret).then(function(result) {
+        if (result.error) {
+          // Display error message in your UI.
+          // The card was declined (i.e. insufficient funds, card has expired, etc)
+          changeLoadingState(false);
+          showCardError(result.error);
+        } else {
+          // Show a success message to your customer
+          confirmSubscription(subscription.id);
+        }
+      });
+    } else {
+      // No additional information was needed
+      // Show a success message to your customer
+      orderComplete(subscription);
+    }
+  } else {
+    orderComplete(subscription);
+  }
+}
+
+function confirmSubscription(subscriptionId) {
+  return fetch('/subscription', {
+    method: 'post',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      subscriptionId: subscriptionId
+    })
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(subscription) {
+      orderComplete(subscription);
+    });
+}
+
+function getPublicKey() {
+  return fetch('/public-key', {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      stripeElements(response.publicKey);
+    });
+}
+
+getPublicKey();
+
+/* ------- Post-payment helpers ------- */
+
+/* Shows a success / error message when the payment is complete */
+var orderComplete = function(subscription) {
+  changeLoadingState(false);
+  var subscriptionJson = JSON.stringify(subscription, null, 2);
+  document.querySelectorAll('.payment-view').forEach(function(view) {
+    view.classList.add('hidden');
+  });
+  document.querySelectorAll('.completed-view').forEach(function(view) {
+    view.classList.remove('hidden');
+  });
+  document.querySelector('.order-status').textContent = subscription.status;
+  document.querySelector('code').textContent = subscriptionJson;
+};
+
+// Show a spinner on subscription submission
+var changeLoadingState = function(isLoading) {
+  if (isLoading) {
+    document.querySelector('#spinner').classList.add('loading');
+    document.querySelector('button').disabled = true;
+
+    document.querySelector('#button-text').classList.add('hidden');
+  } else {
+    document.querySelector('button').disabled = false;
+    document.querySelector('#spinner').classList.remove('loading');
+    document.querySelector('#button-text').classList.remove('hidden');
+  }
+};
